a88d2cea882ddbea2646088bb61a03c198418d13
tcsx <username@users.noreply.github.com>
2018-03-18 20:42:16 -0700

    debug

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,7 @@ import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
+  console.log(res);
   dispatch({ type: FETCH_USER, payload: res.data});
 }; 
 
