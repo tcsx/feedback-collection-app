f775b748c16c0178b1b967148066fe327fa1fb8c
tcsx <username@users.noreply.github.com>
2018-04-12 23:21:28 -0700

    trust proxy

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
   })
 );
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session({proxy: true}));
 
 require('./routes/authRoutes')(app);
 require('./routes/surveyRoutes')(app);
